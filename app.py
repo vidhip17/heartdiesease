a68660b780e992ab41b2d575d075d70630301f4c
vidhi patel <77186108+vidhip17@users.noreply.github.com>
2022-11-02 19:33:46 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+import joblib
+import numpy as np
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello Vidhi Patel"
+
+@app.route('/heart_disease_predict', methods=['POST'])
+def heart_disease_pred():
+    model = joblib.load("./HEART_DISEASE_MODEL.pkl")
+
+    age = request.form.get('age')
+    sex = request.form.get('sex')
+    cp = request.form.get('cp')
+    trestbps = request.form.get('trestbps')
+    chol = request.form.get('chol')
+    fbs = request.form.get('fbs')
+    restecg = request.form.get('restecg')
+    thalach = request.form.get('thalach')
+    exang = request.form.get('exang')
+    oldpeak = request.form.get('oldpeak')
+    slope = request.form.get('slope')
+    ca = request.form.get('ca')
+    thal = request.form.get('thal')
+
+    input = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+    result = model.predict(input)[0]
+    # print(input)
+    # print(result)
+    
+    return json.dumps({"heart_disease":str(result)})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
